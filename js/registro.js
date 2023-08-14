23509b40bbdfa5e2a3215b061344aeba2ba6d4cb
figueredo-870 <140459285+figueredo-870@users.noreply.github.com>
2023-08-14 10:39:46 -0300

    se agrego el retorno al login

--- js/registro.js
+++ js/registro.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
         //Chequea que el usuario no este registrado aun
         if (localStorage.getItem(nombre) !== null) {
             alert('¡El usuario ' + nombre + ' ya está registrado!');
+            window.location.href = "login.html"
             return; // Detener el proceso de registro
         }
         // Guardar los datos en el almacenamiento local
