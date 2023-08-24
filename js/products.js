985a6a3dbc525dbbe62de6eee9ae2e07b024d459
Victoria Martinez <vicotita1999@gmail.com>
2023-08-23 22:24:09 -0300

    Desafiate

--- js/products.js
+++ js/products.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', async () => {
+
+    const btnPrecioAsc=document.getElementById("precioAsc");
+    const btnPrecioDesc=document.getElementById("precioDesc");
+    const btnRelevancia=document.getElementById("relevancia");
+    const btnNoFiltro=document.getElementById("noFiltro");
+    const buscador=document.getElementById("buscador");
+
     // Obtener el valor del localStorage
     const selectedCategoryId = localStorage.getItem("catID");
 
@@ -7,76 +14,84 @@ document.addEventListener('DOMContentLoaded', async () => {
         const response = await getJSONData(PRODUCTS_URL + selectedCategoryId + EXT_TYPE);
         const carList = response.data;
 
+        let productList = [...carList.products];
+        let productListFiltrar = [...carList.products];
+        let filtro = "";
+
         const prodTitle = document.getElementById("titulo");
         prodTitle.innerHTML = `
             <h1> Productos </h1>
             <p>Aqui veras los productos de la categoria ${carList.catName}</p>`;
 
-        let productList = [...carList.products];
-        let productListFiltrar = [...carList.products];
         ListarDatos(productList);
 
-        const btnPrecioAsc=document.getElementById("precioAsc");
-        const btnPrecioDesc=document.getElementById("precioDesc");
-        const btnRelevancia=document.getElementById("relevancia");
-        const btnNoFiltro=document.getElementById("noFiltro");
-        const buscador=document.getElementById("buscador");
+        btnPrecioAsc.addEventListener("click", function(){
+            filtro = "PrecioAsc";
+            filtrar(productList, filtro);
+        });
+        btnPrecioDesc.addEventListener("click",function(){
+            filtro = "PrecioDesc";
+            filtrar(productList, filtro);
+        });
+        btnRelevancia.addEventListener("click",function(){
+            filtro = "Relevancia";
+            filtrar(productList, filtro);
+        });
+        btnNoFiltro.addEventListener("click", function(){
+            filtro = "";
+            container.innerHTML = "";
+            let input = buscador.value.toLowerCase();
+            productList = productListFiltrar.filter(item => item.name.toLowerCase().includes(input) || item.description.toLowerCase().includes(input) || item.currency.toLowerCase().includes(input) || item.cost.toString().includes(input));
+            filtrar(productList, filtro);
+        });
 
         buscador.addEventListener("input", function(){
             let input = buscador.value.toLowerCase();
             productList = productListFiltrar.filter(item => item.name.toLowerCase().includes(input) || item.description.toLowerCase().includes(input) || item.currency.toLowerCase().includes(input) || item.cost.toString().includes(input));
             container.innerHTML = "";
-            ListarDatos(productList);
-        });
-
-        btnPrecioAsc.addEventListener("click", function(){
-            let datosSorteados=productList.sort(function(a,b){
-                if(a.cost>b.cost){
-                    return 1;
-                }    
-                if(a.cost<b.cost){
-                    return -1;
-                }
-                return 0;
-                    
-            })
-            container.innerHTML = "";
-            ListarDatos(datosSorteados);
-            
-        });
-        btnPrecioDesc.addEventListener("click",function(){
-            let datosSorteados=productList.sort(function(a,b){
-                if(a.cost<b.cost){
-                    return 1;
-                }
-                if(a.cost>b.cost){
-                    return -1;
-                }
-                return 0;
-            })
-            container.innerHTML = "";
-            ListarDatos(datosSorteados);
-        });
-        btnRelevancia.addEventListener("click",function(){
-            let datosSorteados=productList.sort(function(a,b){
-                if(a.soldCount<b.soldCount){
-                    return 1;
-                }
-                if(a.soldCount>b.soldCount){
-                    return -1;
-                }
-                return 0;
-            })
-            container.innerHTML = "";
-            ListarDatos(datosSorteados);
-        });
-        btnNoFiltro.addEventListener("click", function(){
-            container.innerHTML = "";
-            ListarDatos(carList.products);
+            filtrar(productList, filtro);
         });
         
     }
 });
+
+function filtrar(array, filtro){
+    let datosSorteados = array;
+    if (filtro === "PrecioDesc") {
+        datosSorteados=array.sort(function(a,b){
+            if(a.cost<b.cost){
+                return 1;
+            }
+            if(a.cost>b.cost){
+                return -1;
+            }
+            return 0;
+        }) 
+    } else if (filtro === "PrecioAsc") {
+        datosSorteados=array.sort(function(a,b){
+            if(a.cost>b.cost){
+                return 1;
+            }    
+            if(a.cost<b.cost){
+                return -1;
+            }
+            return 0;
+                
+        })    
+    } else if (filtro === "Relevancia") {
+        datosSorteados=array.sort(function(a,b){
+            if(a.soldCount<b.soldCount){
+                return 1;
+            }
+            if(a.soldCount>b.soldCount){
+                return -1;
+            }
+            return 0;
+        })
+    }
+    container.innerHTML = "";
+    ListarDatos(datosSorteados);
+}
 
 function ListarDatos(productList){
     const container = document.getElementById('container');
