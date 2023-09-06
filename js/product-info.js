ff5b7c6b4bf44babeb0c806ada987c3618824e1a
MelianGithub <e_melian04@hotmail.com>
2023-09-06 09:13:59 -0300

    product-info.js V1

--- js/product-info.js
+++ js/product-info.js
@@ -1,19 +1,58 @@
-async function productoId() {
+document.addEventListener('DOMContentLoaded', async () => {
+
+
     const selectedId = localStorage.getItem("productoSeleccionado");
     console.log(selectedId);
 
-    try {
-        const responseId = await getJSONData(PRODUCT_INFO_URL + selectedId + EXT_TYPE);
-        console.log(responseId);
-        console.log(typeof responseId)
 
-    } catch (error) {
-        console.error("Error al obtener la información del producto:", error);
-    }
+    const responseId = await getJSONData(PRODUCT_INFO_URL + selectedId + EXT_TYPE);
+    let producto = responseId.data;
+    console.log(producto);
 
-}
+    producto.images.forEach(image => {
+        let imgs = document.createElement('div');
 
-// Habria que crear un for in o algo para recorrer el objeto y despues hacer el append
+        
+    });
 
-productoId();
+    const prod = document.getElementById('info-producto');
+    prod.innerHTML = `
+    <div>
+        <h1>${producto.name}</h1> 
+        <hr>   
+        <p>Precio  <br> ${producto.currency}
+        ${producto.cost} </p>
+        
+        <p>Descripción <br> ${producto.description} </p>
+        <p>Categoría <br>  ${producto.category}</p>
+        <p>Cantidad de vendidos <br> ${producto.soldCount}</p>
+    <p>Imagenes Ilustrativas</p>
+    </div>
+
+    `
+
+
+});
+
+    // Habria que crear un for in o algo para recorrer el objeto y despues hacer el append
+
+
+
+
+
+    // let name = producto.name;
+    // let description = prodList.description;
+    // let price = prodList.cost;
+    // let currency = prodList.currency;
+    // let soldCount = prodList.soldCount;
+    // let image = prodList.image;
+
+    // console.log(name)
+
+
+
+
+
+
+
 
