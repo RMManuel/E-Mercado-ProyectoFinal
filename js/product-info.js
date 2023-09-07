9bd55c0b05896b6c324c19982ef641a1c332439e
Manuel Romero <romero.marichal@gmail.com>
2023-09-07 09:27:58 -0300

    Arreglos varios
    Fotos con seleccion
    Datos
    Comentarios con estrellas
    Inicio FORM comentario

--- js/product-info.js
+++ js/product-info.js
@@ -11,37 +11,55 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     prod.innerHTML = `
     <div class="container">
-        <div class="column" id="principal">
-            <div class="col-md-6">
+        <div id="principal">
+            
+            <div class="info">
+            <div id="Nombre">
+                <hr class="raya">
                 <h1>${producto.name}</h1>
-                <hr>
-                <p><strong>Precio</strong><br> ${producto.currency} ${producto.cost}</p>
-                <p><strong>Descripción</strong><br> ${producto.description}</p>
-                <p><strong>Categoría</strong><br> ${producto.category}</p>
-                <p><strong>Cantidad de vendidos</strong><br> ${producto.soldCount}</p>
+                <hr class="raya">
             </div>
-
-            <div id="imagen">
+                <p class="Titulos"><span style="font-weight: bold;">Precio</span>:<br>${producto.currency}:${producto.cost}</p>
+                <p class="Titulos"><span style="font-weight: bold;">Descripción:</span><br>${producto.description}</p>
+                <p class="Titulos"><span style="font-weight: bold;">Categoría</span>:<br>${producto.category}</p>
+                <p class="Titulos"><span style="font-weight: bold;">Cantidad de vendidos</span>:<br>${producto.soldCount}</p>
             </div>
-
-            <div class="mt-5">
-                <h3 >Comentarios</h3>
-                    <ul id="comentariosParrafo" class="list-group"></ul>
+            <div id="imagen-grande">
             </div>
+            <div id="imagen"></div>
+            </div>
+        <div id="comentariosParrafo">
         </div>
     </div>
-    `;
-
-    const infoProducto = document.getElementById("principal");
-    const imagen = document.getElementById("imagen");
-    prod.appendChild(infoProducto);
-
-    producto.images.forEach(image => {
-        let img = document.createElement('img');
-        img.src = image;
-        img.classList.add('img-fluid', 'col-2', 'm-2');
-        imagen.appendChild(img);
+        `;
+    //figueredo linea 12 al 27 por puteadas 
+    const imagenGrande = document.getElementById("imagen-grande");
+    const imagenContainer = document.getElementById("imagen");
+    const imagenes = producto.images;
+    let indiceImagenActual = 0;
+    
+    
+    function mostrarImagenGrande() {
+    imagenGrande.innerHTML = `<img id="fotogrande" src="${imagenes[indiceImagenActual]}" alt="Imagen grande">`;
+    }
+    
+        
+    mostrarImagenGrande();
+    
+        
+    imagenes.forEach((image, index) => {
+    let img = document.createElement('img');
+    img.src = image;
+    img.id = `imagen-${index}`;
+    imagenContainer.appendChild(img);
+    
+     //para funcion click foto figueredo
+    img.addEventListener('click', () => {
+        indiceImagenActual = index;
+        mostrarImagenGrande();
     });
+    });
+
     
 
     //Esto es lo que agregu'e para traer los comentarios
@@ -60,18 +78,42 @@ document.addEventListener('DOMContentLoaded', async () => {
         let date = comentario.dateTime;
         let score = comentario.score;
         let description = comentario.description;
+        let stars = "";
+        
+        for(var i = 1; i <= score; i++) {
+            stars += `
+            <i class="fa fa-star checked"></i>`
+        }
+        for(var j = 1; j <= (5-score); j++) {
+            stars += `
+            <i class="fa fa-star"></i>`
+        }
 
         listItem.innerHTML = `
             <strong>${name} </strong> 
             ${date}
             ${score}
+            ${stars}
             <p>${description}</p>
+            
         `;
 
         listaComentarios.appendChild(listItem);
     });
 
     let comentariosParrafo = document.getElementById('comentariosParrafo');
+    listaComentarios.innerHTML+=`
+    <div id="cajacomentarios">
+    <h3>Comentar</h3>
+    <label for="opinion">Tu opinión:</label><textarea name="opinion" id="txtareaOpinion" cols="30" rows="3"></textarea>
+    <label for="selPuntaje">Tu puntuación</label><select name="selPuntaje" id="selectionPuntaje">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+    </select>
+    </div>`
     comentariosParrafo.appendChild(listaComentarios);
 
 });
