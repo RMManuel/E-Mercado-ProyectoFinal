d62af5b512483e3c347a7d899cfc1d4c3b8cd8f4
Victoria Martinez <vicotita1999@gmail.com>
2023-09-12 16:05:32 -0300

    Arreglos visuales

--- js/product-info.js
+++ js/product-info.js
@@ -1,32 +1,26 @@
 document.addEventListener('DOMContentLoaded', async () => {
 
-    const prod = document.getElementById('cajaproducto');
+    const prod = document.getElementById('principal');
     const selectedId = localStorage.getItem("productoSeleccionado");
-    console.log(selectedId);
     const responseId = await getJSONData(PRODUCT_INFO_URL + selectedId + EXT_TYPE);
     let producto = responseId.data;
 
     prod.innerHTML += `
-    
-        <div id="principal">
-            
-            <div class="info">
+        <div class="info">
             <div id="Nombre">
-                <hr class="raya">
+                <hr>
                 <h1>${producto.name}</h1>
-                <hr class="raya">
+                <hr>
             </div>
-                <p class="Titulos"><span style="font-weight: bold;">Precio</span>:<br>${producto.currency}:${producto.cost}</p>
-                <p class="Titulos"><span style="font-weight: bold;">Descripción:</span><br>${producto.description}</p>
-                <p class="Titulos"><span style="font-weight: bold;">Categoría</span>:<br>${producto.category}</p>
-                <p class="Titulos"><span style="font-weight: bold;">Cantidad de vendidos</span>:<br>${producto.soldCount}</p>
-            </div>
-            <div id="imagen-grande">
-            </div>
-            <div id="imagen"></div>
-            </div>
-    
-        `;
+            <p><span class="bold">Precio</span>:<br>${producto.currency}:${producto.cost}</p>
+            <p><span class="bold">Descripción:</span><br>${producto.description}</p>
+            <p><span class="bold">Categoría</span>:<br>${producto.category}</p>
+            <p><span class="bold">Cantidad de vendidos</span>:<br>${producto.soldCount}</p>
+        </div>
+        <div id="imagen-grande">
+        </div>
+        <div id="imagen">
+        </div>`;
 
     //IMAGENES
     const imagenGrande = document.getElementById("imagen-grande");
@@ -37,92 +31,85 @@ document.addEventListener('DOMContentLoaded', async () => {
     function mostrarImagenGrande() {
     imagenGrande.innerHTML = `<div id="fotogrande"><img src="${imagenes[indiceImagenActual]}" alt="Imagen grande"></div>`;
     }
-     
+    
     mostrarImagenGrande();
-       
+    
     imagenes.forEach((image, index) => {
     let img = document.createElement('img');
     img.src = image;
     img.id = `imagen-${index}`;
     imagenContainer.appendChild(img);
     
-     //para funcion click foto figueredo
+    //Función click foto
     img.addEventListener('click', () => {
         indiceImagenActual = index;
         mostrarImagenGrande();
     });
     });
 
-     //COMENTARIOS
+    //COMENTARIOS
     const obtenerComentarios = await getJSONData(PRODUCT_INFO_COMMENTS_URL + selectedId + EXT_TYPE);
     let comentarios = obtenerComentarios.data;
-    console.log(comentarios);
-    ListarComentarios(comentarios)
+    listarComentarios(comentarios);
 
-    
-     
+    let btn = document.getElementById("btnEnviar");
 
-    let btn=document.getElementById("btnEnviar");
-    btn.addEventListener("click",()=>{
-        let opinion=document.getElementById("txtareaOpinion").value;
-        let puntuación=document.getElementById("selectionPuntaje").value;
-        let fecha=new Date().toLocaleString()
-        localStorage.setItem('opinion', opinion);
-        localStorage.setItem('puntuacion',puntuación);
-        let comentarioNuevo={
+    btn.addEventListener("click", () => {
+        let opinion = document.getElementById("txtareaOpinion").value;
+        let puntuación = document.getElementById("selectionPuntaje").value;
+        let fecha = new Date().toLocaleDateString('en-US');
+        let comentarioNuevo = {
             product: localStorage.getItem('productoSeleccionado'),
             score: puntuación,
             description: opinion,
             user: localStorage.getItem('username'),
             dateTime: fecha
+        };
 
-        }
         comentarios.push(comentarioNuevo);
-        ListarComentarios(comentarios);
+        document.getElementById("txtareaOpinion").value = "";
+        document.getElementById("selectionPuntaje").value = "1";
+        listarComentarios(comentarios);
         
-
     })
 
 })
 
-function ListarComentarios(comentarios){
-   let listaComentarios = document.createElement('ul');
+function listarComentarios(comentarios){
 
-   listaComentarios.classList.add('list-group');
+    let listaComentarios = document.createElement('ul');
 
-   comentarios.forEach(comentario => {
-       comentariosParrafo.innerHTML=`
-       <hr>
-       <h3>Comentarios</h3>
-       `; 
-       let listItem = document.createElement('li');
-       listItem.classList.add('list-group-item');
-       let name = comentario.user;
-       let date = comentario.dateTime;
-       let score = comentario.score;
-       let description = comentario.description;
-       let stars = "";
-       
-       for(var i = 1; i <= score; i++) {
-           stars += `
-           <i class="fa fa-star checked"></i>`
-       }
-       for(var j = 1; j <= (5-score); j++) {
-           stars += `
-           <i class="fa fa-star"></i>`
-       }
+    listaComentarios.classList.add('list-group');
 
-       listItem.innerHTML = `
-           <strong>${name} </strong> 
-           <small class="text-muted">${date} </small>
-           ${score}
-           ${stars}
-           <p>${description}</p>
-           
-       `;
+    comentarios.forEach(comentario => {
+        comentariosParrafo.innerHTML = `
+            <hr>
+            <h3>Comentarios</h3>`; 
 
-       listaComentarios.appendChild(listItem);
-       comentariosParrafo.appendChild(listaComentarios);
-   })
+        let listItem = document.createElement('li');
+        listItem.classList.add('list-group-item');
+
+        let name = comentario.user;
+        let date = new Date(comentario.dateTime).toLocaleDateString();
+        let score = comentario.score;
+        let description = comentario.description;
+        let stars = "";
+        
+        for(var i = 1; i <= score; i++) {
+            stars += `<i class="fa fa-star checked"></i>`;
+        }
+        for(var j = 1; j <= (5-score); j++) {
+            stars += `<i class="fa fa-star"></i>`;
+        }
+
+        listItem.innerHTML = `
+            <strong>${name} </strong> 
+            <small class="text-muted">${date} </small>
+            ${score} ${stars}
+            <p>${description}</p>`;
+
+        listaComentarios.appendChild(listItem);
+        comentariosParrafo.appendChild(listaComentarios);
+    })
 }
 
