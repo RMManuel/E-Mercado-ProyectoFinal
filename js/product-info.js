da3fca9f2bcf8e8e1ee62010cea51f46f7d80303
Manuel Romero <romero.marichal@gmail.com>
2023-09-12 12:28:11 -0300

    Arreglos Varios
    Product Info con cambios en funciones, estilo, etc. Funcionalidad completa de entrega 3.

--- js/product-info.js
+++ js/product-info.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', async () => {
 
-    const prod = document.getElementById('info-producto');
-
+    const prod = document.getElementById('cajaproducto');
     const selectedId = localStorage.getItem("productoSeleccionado");
     console.log(selectedId);
-
     const responseId = await getJSONData(PRODUCT_INFO_URL + selectedId + EXT_TYPE);
     let producto = responseId.data;
-    console.log(producto);
 
-    prod.innerHTML = `
-    <div class="container">
+    prod.innerHTML += `
+    
         <div id="principal">
             
             <div class="info">
@@ -28,25 +25,21 @@ document.addEventListener('DOMContentLoaded', async () => {
             </div>
             <div id="imagen"></div>
             </div>
-        <div id="comentariosParrafo">
-        </div>
-    </div>
+    
         `;
-    //figueredo linea 12 al 27 por puteadas 
+
+    //IMAGENES
     const imagenGrande = document.getElementById("imagen-grande");
     const imagenContainer = document.getElementById("imagen");
     const imagenes = producto.images;
     let indiceImagenActual = 0;
     
-    
     function mostrarImagenGrande() {
     imagenGrande.innerHTML = `<div id="fotogrande"><img src="${imagenes[indiceImagenActual]}" alt="Imagen grande"></div>`;
     }
-    
-        
+     
     mostrarImagenGrande();
-    
-        
+       
     imagenes.forEach((image, index) => {
     let img = document.createElement('img');
     img.src = image;
@@ -60,60 +53,76 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
     });
 
-    
-
-    //Esto es lo que agregu'e para traer los comentarios
+     //COMENTARIOS
     const obtenerComentarios = await getJSONData(PRODUCT_INFO_COMMENTS_URL + selectedId + EXT_TYPE);
     let comentarios = obtenerComentarios.data;
     console.log(comentarios);
+    ListarComentarios(comentarios)
 
-    let listaComentarios = document.createElement('ul');
+    
+     
 
-    listaComentarios.classList.add('list-group');
+    let btn=document.getElementById("btnEnviar");
+    btn.addEventListener("click",()=>{
+        let opinion=document.getElementById("txtareaOpinion").value;
+        let puntuación=document.getElementById("selectionPuntaje").value;
+        let fecha=new Date().toLocaleString()
+        localStorage.setItem('opinion', opinion);
+        localStorage.setItem('puntuacion',puntuación);
+        let comentarioNuevo={
+            product: localStorage.getItem('productoSeleccionado'),
+            score: puntuación,
+            description: opinion,
+            user: localStorage.getItem('username'),
+            dateTime: fecha
 
-    comentarios.forEach(comentario => {
-        let listItem = document.createElement('li');
-        listItem.classList.add('list-group-item');
-        let name = comentario.user;
-        let date = comentario.dateTime;
-        let score = comentario.score;
-        let description = comentario.description;
-        let stars = "";
+        }
+        comentarios.push(comentarioNuevo);
+        ListarComentarios(comentarios);
         
-        for(var i = 1; i <= score; i++) {
-            stars += `
-            <i class="fa fa-star checked"></i>`
-        }
-        for(var j = 1; j <= (5-score); j++) {
-            stars += `
-            <i class="fa fa-star"></i>`
-        }
 
-        listItem.innerHTML = `
-            <strong>${name} </strong> 
-            <small class="text-muted">${date} </small>
-            ${score}
-            ${stars}
-            <p>${description}</p>
-            
-        `;
+    })
 
-        listaComentarios.appendChild(listItem);
-    });
+})
 
-    let comentariosParrafo = document.getElementById('comentariosParrafo');
-    listaComentarios.innerHTML+=`
-    <div id="cajacomentarios">
-    <h3>Comentar</h3>
-    <label for="opinion">Tu opinión:</label><textarea name="opinion" id="txtareaOpinion" cols="30" rows="3"></textarea>
-    <label for="selPuntaje">Tu puntuación</label><select name="selPuntaje" id="selectionPuntaje">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-    </select>
-    </div>`
-    comentariosParrafo.appendChild(listaComentarios);
+function ListarComentarios(comentarios){
+   let listaComentarios = document.createElement('ul');
 
-});
+   listaComentarios.classList.add('list-group');
+
+   comentarios.forEach(comentario => {
+       comentariosParrafo.innerHTML=`
+       <hr>
+       <h3>Comentarios</h3>
+       `; 
+       let listItem = document.createElement('li');
+       listItem.classList.add('list-group-item');
+       let name = comentario.user;
+       let date = comentario.dateTime;
+       let score = comentario.score;
+       let description = comentario.description;
+       let stars = "";
+       
+       for(var i = 1; i <= score; i++) {
+           stars += `
+           <i class="fa fa-star checked"></i>`
+       }
+       for(var j = 1; j <= (5-score); j++) {
+           stars += `
+           <i class="fa fa-star"></i>`
+       }
+
+       listItem.innerHTML = `
+           <strong>${name} </strong> 
+           <small class="text-muted">${date} </small>
+           ${score}
+           ${stars}
+           <p>${description}</p>
+           
+       `;
+
+       listaComentarios.appendChild(listItem);
+       comentariosParrafo.appendChild(listaComentarios);
+   })
+}
+
