03dcb0a618947dff20c685a6d85d56d74b4b1e2c
MelianGithub <e_melian04@hotmail.com>
2023-09-26 06:27:36 -0300

    Varios arreglos + DarkMode

---
+++ js/theme.js
@@ -0,0 +1,26 @@
+let darkLight = document.getElementById("toggle-mode");
+
+function saveThemeToLocalStorage(theme) {
+    localStorage.setItem("theme", theme);
+}
+
+function loadThemeFromLocalStorage() {
+    return localStorage.getItem("theme");
+}
+
+darkLight.addEventListener("click", () => {
+    console.log("hiciste click");
+
+    let element = document.body;
+    element.dataset.bsTheme = element.dataset.bsTheme == "light" ? "dark" : "light";
+    saveThemeToLocalStorage(element.dataset.bsTheme);
+});
+
+// Verificar el tema en el localStorage y aplicarlo solo si existe al cargar la página.
+document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = loadThemeFromLocalStorage();
+    if (savedTheme) {
+        let element = document.body;
+        element.dataset.bsTheme = savedTheme;
+    }
+});
