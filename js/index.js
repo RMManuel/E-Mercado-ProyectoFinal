4cc6fcec3d5cec679b62f9fbada23a78cc6722d0
MelianGithub <e_melian04@hotmail.com>
2023-09-26 07:07:14 -0300

    intentando solucionar errores
    Estoy intentando solucionar errores de la consola.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const isLoggedIn = localStorage.getItem('isLoggedIn');
   const username = localStorage.getItem('username');
   const usernameElement = document.getElementById('username');
@@ -16,36 +16,38 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   // Agregar un evento al botón de cerrar sesión para eliminar la información de inicio de sesión y redireccionar a la página de login.
-  logoutButton.addEventListener('click', function() {
+  logoutButton.addEventListener('click', function () {
     localStorage.removeItem('isLoggedIn');
     localStorage.removeItem('username');
     window.location.href = "login.html";
   });
-}); 
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+
+
+  document.getElementById("autos").addEventListener("click", function () {
+    localStorage.setItem("catID", 101);
+    window.location = "products.html"
+  });
+  document.getElementById("juguetes").addEventListener("click", function () {
+    localStorage.setItem("catID", 102);
+    window.location = "products.html"
+  });
+  document.getElementById("muebles").addEventListener("click", function () {
+    localStorage.setItem("catID", 103);
+    window.location = "products.html"
+  });
 });
 
 
-  
-  
-  
-    
 
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
