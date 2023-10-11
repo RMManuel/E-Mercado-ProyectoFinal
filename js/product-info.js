34b71a41fa1395a2de1034b5375a6f1b0ccdb8ce
MelianGithub <e_melian04@hotmail.com>
2023-10-11 08:32:27 -0300

    Responsive

--- js/product-info.js
+++ js/product-info.js
@@ -35,7 +35,7 @@ function mostrarProductoSeleccionado(producto) {
             <h1>${producto.name}</h1>
             <hr>
         </div>
-        <div id="imagen-grande"></div>  
+          
         <p><span class="bold">Precio</span>:<br>${producto.currency}:${producto.cost}</p>
         <p><span class="bold">Descripción:</span><br>${producto.description}</p>
         <p><span class="bold">Categoría</span>:<br>${producto.category}</p>
@@ -43,7 +43,7 @@ function mostrarProductoSeleccionado(producto) {
         <button id="agregarAlCarrito" onclick='agregarAlCarrito(${JSON.stringify(producto)})'>Agregar al carrito</button>
         </div>
     
-    
+        <div id="imagen-grande"></div>
     `;
 
 
