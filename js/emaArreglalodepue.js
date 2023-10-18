3fe887a0f886d15d7606f6179309a9c183ae5dbb
figueredo-870 <cristianfigueredo870@outlook.com>
2023-10-18 08:44:16 -0300

    arreglo pago fantasma

--- js/emaArreglalodepue.js
+++ js/emaArreglalodepue.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var radioTarjeta = document.getElementById('radioTarjeta');
     var radioTransferencia = document.getElementById('radioTransferencia');
-    
 
     function actualizarCampos() {
         var camposTarjeta = document.querySelectorAll('#dato1, #dato2, #fecha, #dato4');
@@ -15,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
             campo.disabled = !radioTransferencia.checked;
         });
     }
-    
 
     // Ejecuta la función inicialmente para establecer el estado inicial
     actualizarCampos();
@@ -30,8 +28,38 @@ document.addEventListener('DOMContentLoaded', function() {
     var formularioPago = document.getElementById('formularioPago');
     if (formularioPago) {
         formularioPago.addEventListener('submit', function(event) {
-            event.preventDefault(); // Evita que el formulario se envíe realmente
+            //  que este seleccionado una opcio
+            if (!radioTarjeta.checked && !radioTransferencia.checked) {
+                event.preventDefault();
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Opción no seleccionada',
+                    text: 'Por favor, selecciona una opción de pago.',
+                });
+                return;
+            }
 
+            var campoCodigo = document.getElementById('dato4');
+
+            // validacione tarjeta
+            if (radioTarjeta.checked) {
+                var camposTarjeta = document.querySelectorAll('#dato1, #dato2, #fecha, #dato4');
+                var camposVacios = Array.from(camposTarjeta).some(function(campo) {
+                    return campo.value.trim() === '';
+                });
+
+                if (camposVacios) {
+                    event.preventDefault();
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Campos incompletos',
+                        text: 'Por favor, completa todos los campos de tarjeta.',
+                    });
+                    return;
+                }
+            }
+
+            event.preventDefault(); // cancela envio
             // Simulación de verificación de pago
             var exitoPago = Math.random() < 0.5; // Simula un 50% de éxito
 
@@ -41,6 +69,10 @@ document.addEventListener('DOMContentLoaded', function() {
                     title: '¡Pago exitoso!',
                     text: 'Gracias por tu pago.',
                 });
+
+                // reset forzado
+                formularioPago.reset();
+                actualizarCampos(); // actualiza los campos
             } else {
                 Swal.fire({
                     icon: 'error',
@@ -52,5 +84,4 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         console.error('Elemento de formulario no encontrado con el ID proporcionado.');
     }
-}
-);
+});
