2c136bed2cc69781ec732391ef1019535b7404a9
MelianGithub <e_melian04@hotmail.com>
2023-11-07 06:29:49 -0300

    cargando datos predeterminados
    estoy empezando la funcion para que si ya se modificó aparezcan loss datos.

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,19 +1,37 @@
 let btnsave = document.getElementById("save");
 let email = document.getElementById("email");
 let username = localStorage.getItem('username');
+
 email.value = JSON.parse(localStorage.getItem(username)).mail;
 
+
 document.addEventListener("DOMContentLoaded", () => {
-    console.log(email);
+
     btnsave.addEventListener("click", () => {
+        // cargarDatosUser()
         validar();
     });
+
+   
+
 });
 
-function validar () {
-let name = document.getElementById("name").value;
-let lastname = document.getElementById("lastname").value;
-let email = document.getElementById("email").value; 
+
+// function cargarDatosUser() {
+   
+// //hay que agregar un condicional pa que si el email esta registrado, me llene los imputs con lo que sea, y sino que no llene nada
+// }
+
+
+function validar() {
+    let name = document.getElementById("name").value;
+    let lastname = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
+    let secondName = document.getElementById("secondName").value;
+    let lastName2 = document.getElementById("lastname2").value;
+
+    
+
     if ((name == "") || (lastname == "") || (email == "")) {
         Swal.fire({
             position: "top",
@@ -22,5 +40,26 @@ let email = document.getElementById("email").value;
             text: "Debe completar los campos obligatorios"
         });
     }
+    else {
+        Swal.fire({
+            position: "top",
+            icon: "success",
+            title: "",
+            text: "Se guardaron los cambios"
+        });
+
+        let datosUsuario = {
+            name: name,
+            lastname: lastname,
+            email: email,
+            secondName: secondName,
+            lastName2: lastName2
+
+        }
+        // NO SE QUE CLAVE PONER ACA PARA VALIDAR
+        localStorage.setItem("no se que poner acaa", JSON.stringify(datosUsuario))
+       
+    }
+
 
 }
