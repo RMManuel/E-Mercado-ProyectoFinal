9abf37b310a924e4254ab76ec5cf675fd77c89e4
MelianGithub <e_melian04@hotmail.com>
2023-11-08 10:44:23 -0300

    Codigo funcional sin foto perfil
    Vuelvo al codigo anterior porque el de la img da error

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,11 +1,16 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     cargarDatosUser();
 
-    let btnsave = document.getElementById("save");
     btnsave.addEventListener("click", () => {
+
         validar();
     });
+
+
 });
+
+
 
 let btnsave = document.getElementById("save");
 let email = document.getElementById("email");
@@ -13,42 +18,47 @@ let username = localStorage.getItem('username');
 
 email.value = JSON.parse(localStorage.getItem(username)).mail;
 
-let nombre = document.getElementById("name");
-let lastname = document.getElementById("lastname");
-let secondName = document.getElementById("secondName");
-let lastName2 = document.getElementById("lastname2");
-let contacto = document.getElementById("contacto");
-let imgPerfil = document.getElementById('imgPerfil');
-let nuevaFoto = document.getElementById('inputGroupFile04');
+
+
+let nombre = document.getElementById("name")
+let lastname = document.getElementById("lastname")
+// let email = document.getElementById("email").value;
+let secondName = document.getElementById("secondName")
+let lastName2 = document.getElementById("lastname2")
+let contacto = document.getElementById("contacto")
+let imgPerfil = document.getElementById('imgPerfil')
+let nuevaFoto = document.getElementById('inputGroupFile04')
+
 
 function cargarDatosUser() {
+
+
     for (const prop in localStorage) {
         if (prop == email.value) {
-            let user = JSON.parse(localStorage.getItem(prop));
+            let user = JSON.parse(localStorage.getItem(prop))
 
             nombre.value = user.name;
             lastname.value = user.lastname;
             secondName.value = user.secondName;
             lastName2.value = user.lastName2;
             contacto.value = user.contacto;
-            imgPerfil.src = user.nuevaFoto;
+            // imgPerfil.src = user.nuevaFoto
         }
     }
 }
 
+
 function validar() {
-    if (
-        nombre.value === "" ||
-        lastname.value === "" ||
-        email.value === ""
-    ) {
+
+    if ((nombre.value == "") || (lastname.value == "") || (email.value == "")) {
         Swal.fire({
             position: "top",
             icon: "error",
             title: "Oops...",
             text: "Debe completar los campos obligatorios"
         });
-    } else {
+    }
+    else {
         Swal.fire({
             position: "top",
             icon: "success",
@@ -56,26 +66,20 @@ function validar() {
             text: "Se guardaron los cambios"
         });
 
-        const file = nuevaFoto.files[0];
-        const reader = new FileReader();
-        reader.onload = function(event) {
-            let base64Image = event.target.result;
+        let datosUsuario = {
+            name: nombre.value,
+            lastname: lastname.value,
+            email: email.value,
+            secondName: secondName.value,
+            lastName2: lastName2.value,
+            contacto: contacto.value,
+            // nuevaFoto: nuevaFoto.value
 
-            let datosUsuario = {
-                name: nombre.value,
-                lastname: lastname.value,
-                email: email.value,
-                secondName: secondName.value,
-                lastName2: lastName2.value,
-                contacto: contacto.value,
-                nuevaFoto: base64Image // Guarda la imagen en formato base64
-            };
-
-            localStorage.setItem(email.value, JSON.stringify(datosUsuario));
-        };
-
-        if (file) {
-            reader.readAsDataURL(file);
         }
+        // NO SE QUE CLAVE PONER ACA PARA VALIDAR
+        localStorage.setItem(email.value, JSON.stringify(datosUsuario))
+
     }
+
+
 }
